8e55652fa23980f548f8cabaa9576cdc533d303b
Jerome Bazin <jerome.bazin@gmx.com>
2017-07-09 21:22:11 +0200

    Ok, Modifying Splash and transition from splash too. No background process involved in the splash.

--- app/src/main/java/jbazin/gberhault/gardenmate/SplashActivity.java
+++ app/src/main/java/jbazin/gberhault/gardenmate/SplashActivity.java
@@ -3,6 +3,7 @@ package jbazin.gberhault.gardenmate;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 
 /**
@@ -13,9 +14,22 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                splashBackgroundProcessDone();
+            }
+        }, 3000);
+    }
+
+    private void splashBackgroundProcessDone() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        overridePendingTransition(R.anim.zoom_enter_new ,R.anim.zoom_exit_actual);
         finish();
     }
 }
